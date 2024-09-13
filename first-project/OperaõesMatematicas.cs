969096e41957251cc4721b36df927817e1f3e0b8
Gabriel Jorge <gabriel.ggcj@gmail.com>
2024-09-13 02:42:06 -0300

    new/update

---
+++ first-project/OperaõesMatematicas.cs
@@ -0,0 +1,11 @@
+﻿namespace first_project
+{
+    public class OperaõesMatematicas
+    {
+        public double Somar(double valor1, double valor2) => valor1 + valor2;
+        public double Subtrair(double valor1, double valor2) => valor1 - valor2 ;
+        public double Multiplicar(double valor1, double valor2) => valor1 * valor2 ;
+        public double Dividir(double valor1, double valor2) => valor1 / valor2 ;
+        public double Media(double valor1, double valor2) => (valor1 + valor2) / 2 ;
+    }
+}
